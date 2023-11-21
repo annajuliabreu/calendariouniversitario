c9a089ed2c68a25bd166314581c94141a5dc078f
Anderson Soares <andersonlbsoares@alu.ufc.br>
2023-11-21 17:21:50 -0300

    update.

--- MeuCalendarioUniversitario/components/MainPage.jsx
+++ MeuCalendarioUniversitario/components/MainPage.jsx
@@ -1,6 +1,6 @@
-import Home from "AgendaPersonalizada/Home.jsx"
+import Home from "./AgendaPersonalizada/Home.jsx"
 
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -10,9 +10,9 @@ const NativeStack = createNativeStackNavigator()
 const MainPageProva = () => {
     return (
         <View>
+            <Text>Teste</Text>
             <NavigationContainer>
                 <NativeStack.Navigator screenOptions={{ headerShown: false }}>
-
                     <NativeStack.Group>
                         <NativeStack.Screen name="Agenda Personalizada" component={Home} />
                     </NativeStack.Group>
